0e7d7215527a3d8cb799efa916c9af3de32d47ae
George McEntegart <geerodge@gmail.com>
2022-12-06 13:51:07 +0000

    Update ssg.php

--- config/statamic/ssg.php
+++ config/statamic/ssg.php
@@ -41,7 +41,7 @@ return [
     'copy' => [
         public_path('css') => 'css',
         public_path('js') => 'js',
-        public_path('assets') => 'public/assets',
+        public_path('assets') => 'assets',
         public_path('fonts') => 'fonts',
         public_path('robots.txt') => 'robots.txt',
         public_path('favicon.ico') => 'favicon.ico',
