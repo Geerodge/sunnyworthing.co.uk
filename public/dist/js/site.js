44dab95a65328e9f25367b98185d6931a673be79
George McEntegart <geerodge@gmail.com>
2023-01-04 21:24:00 +0000

    Updates

--- public/dist/js/site.js
+++ public/dist/js/site.js
@@ -684,50 +684,53 @@ else document.getElementsByName("email_address")[0].placeholder = "Enter email a
 }(document);
 
 },{}],"aaK6T":[function(require,module,exports) {
-const mobileNavigation = document.querySelector(".js-mobile-navigation");
+const mobileNav = document.querySelector(".js-mobile-nav");
 const hamburgerMenu = document.querySelectorAll(".js-hamburger-menu");
-const navbarLinks = document.querySelectorAll(".js-anchor");
+// const navbarLinks = document.querySelectorAll(".js-anchor");
 let mobileMenuOpen = false;
 hamburgerMenu.forEach((menu)=>{
     menu.addEventListener("click", ()=>{
         mobileMenuOpen = !mobileMenuOpen;
         if (mobileMenuOpen) {
-            mobileNavigation.classList.remove("nav__mobile-closed");
-            mobileNavigation.classList.add("nav__mobile-open");
+            mobileNav.classList.remove("mobile-nav-closed");
+            mobileNav.classList.add("mobile-nav-open");
         } else {
-            mobileNavigation.classList.add("nav__mobile-closed");
-            mobileNavigation.classList.remove("nav__mobile-open");
+            mobileNav.classList.add("mobile-nav-closed");
+            mobileNav.classList.remove("mobile-nav-open");
         }
     });
-});
-navbarLinks.forEach((elem)=>elem.addEventListener("click", navbarLinkClick));
-function navbarLinkClick(event) {
-    if (window.location.pathname === "/") smoothScroll(event);
-    else document.location.href = "/";
-}
-function smoothScroll(event) {
-    event.preventDefault();
-    const targetId = event.currentTarget.getAttribute("href");
-    const targetPosition = document.querySelector(targetId).offsetTop;
-    const startPosition = window.pageYOffset;
-    const distance = targetPosition - startPosition;
-    const duration = 1000;
-    let start = null;
-    window.requestAnimationFrame(step);
-    function step(timestamp) {
-        if (!start) start = timestamp;
-        const progress = timestamp - start;
-        window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
-        if (progress < duration) window.requestAnimationFrame(step);
-    }
-}
-// Easing Functions
-function easeInOutCubic(t, b, c, d) {
-    t /= d / 2;
-    if (t < 1) return c / 2 * t * t * t + b;
-    t -= 2;
-    return c / 2 * (t * t * t + 2) + b;
-}
+}); // navbarLinks.forEach((elem) => elem.addEventListener("click", navbarLinkClick));
+ // function navbarLinkClick(event) {
+ //     if (window.location.pathname === "/") {
+ //         smoothScroll(event);
+ //     } else document.location.href = "/";
+ // }
+ // function smoothScroll(event) {
+ //     event.preventDefault();
+ //     const targetId = event.currentTarget.getAttribute("href");
+ //     const targetPosition = document.querySelector(targetId).offsetTop;
+ //     const startPosition = window.pageYOffset;
+ //     const distance = targetPosition - startPosition;
+ //     const duration = 1000;
+ //     let start = null;
+ //     window.requestAnimationFrame(step);
+ //     function step(timestamp) {
+ //         if (!start) start = timestamp;
+ //         const progress = timestamp - start;
+ //         window.scrollTo(
+ //             0,
+ //             easeInOutCubic(progress, startPosition, distance, duration)
+ //         );
+ //         if (progress < duration) window.requestAnimationFrame(step);
+ //     }
+ // }
+ // // Easing Functions
+ // function easeInOutCubic(t, b, c, d) {
+ //     t /= d / 2;
+ //     if (t < 1) return (c / 2) * t * t * t + b;
+ //     t -= 2;
+ //     return (c / 2) * (t * t * t + 2) + b;
+ // }
 
 },{}]},["4u3L3","7YkBc"], "7YkBc", "parcelRequire2e80")
 
